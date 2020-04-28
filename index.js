8b0cf960b8853a3bce72bb8d54dccb8f60275737
EnriqueEOS <64421257+EnriqueEOS@users.noreply.github.com>
2020-04-28 13:17:52 -0500

    Update index.js
    Test to see if code updates on Lambda

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda and Github! This test worked!'),
+        //First Commit From GitHub 
+        //Test
     };
     return response;
 };
